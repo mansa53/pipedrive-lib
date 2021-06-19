a57db8172076ff437de5a21a3d157d60e38c6977
mansa53 <31716134+mansa53@users.noreply.github.com>
2021-06-19 12:22:33 +0530

    Add files via upload

--- pipedrive/utils.py
+++ pipedrive/utils.py
@@ -5,6 +5,7 @@ class Utils(object):
     def getPersonDetails(self,data):
         response = {}
         response['Action'] = data['meta']['action']
+        ## 12 is internal code for Yes and 13 for No for the custom field
         response['SIQ Stop'] = 'True' if data['current']['1d49338625a9066d827b531176c589959de0a595'] == 12 else 'False'
         response['details'] = data['current']
         json.dumps(response)
